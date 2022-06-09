ec725a85f3a983e581da3c98be0c865241a5cae2
Gersi Muca <gm96key@gmail.com>
2022-06-09 12:31:56 +0200

    More details

--- src/main/java/com/gersimuca/webapp/controllers/BookController.java
+++ src/main/java/com/gersimuca/webapp/controllers/BookController.java
@@ -3,16 +3,26 @@ package com.gersimuca.webapp.controllers;
 import com.gersimuca.webapp.entities.Book;
 import com.gersimuca.webapp.respositories.BookRepository;
 import com.gersimuca.webapp.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Controller
 public class BookController {
+    @Autowired
     private BookService bookService;
 
     @PostMapping("/create-book")
-    public Book createBook(Book book){
-        return bookService.createBook(book);
+    public Book createBook(@RequestBody Book book) { return bookService.createBook(book);
     }
 
+    @GetMapping("/get-books")
+    public List<Book> getAll(){
+        List<Book> books = bookService.getAllBooks();
+        return books;
+    }
 }
--- src/main/java/com/gersimuca/webapp/services/BookService.java
+++ src/main/java/com/gersimuca/webapp/services/BookService.java
@@ -2,17 +2,23 @@ package com.gersimuca.webapp.services;
 
 import com.gersimuca.webapp.entities.Book;
 import com.gersimuca.webapp.respositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
-    private final BookRepository bookRepository;
-    public BookService(BookRepository bookRepository){
-        this.bookRepository = bookRepository;
-    }
+    @Autowired
+    private BookRepository bookRepository;
 
     public Book createBook(Book book){
         bookRepository.save(book);
         return  book;
+    }
+
+    public List<Book> getAllBooks(){
+        List<Book> books = bookRepository.findAll();
+        return books;
     }
 }
